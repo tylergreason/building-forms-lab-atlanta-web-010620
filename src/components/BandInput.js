e0577128076100e6b5a94c7909e7c6a3e703f8fa
tylergreason <tyler.greason@protonmail.com>
2020-03-27 17:22:48 -0400

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,51 @@
 // Add BandInput component
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class BandInput extends Component {
+  state = {
+    name:''
+  }
+
+  handleInput = e => {
+    this.setState({
+      name:e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log(this.state)
+    this.props.addBand(this.state)
+    this.setState({
+      name:''
+    })
+  }
+
   render() {
     return(
       <div>
+        <form
+          onSubmit={(e) => this.handleSubmit(e)}
+          >
+          <input 
+            type='text'
+            value={this.state.name}
+            onChange={(e) => this.handleInput(e)}
+          />
+          <input type="submit" />
+
+        </form>
         Band Input
       </div>
     )
   }
 }
+const mapStateToProps = state =>{
+  return {
+      bands: state.bands
+  }
+}
 
+// export default connect(mapStateToProps)(BandInput)
 export default BandInput
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import BandInput from '../components/BandInput'
 
 class BandsContainer extends Component {
+  state = {
+    bands:[]
+  }
+
+  renderBands = (bands, index) => {
+    return bands.map(band => <li key={index}>{band.name}</li>)
+  }
+
+
   render() {
     return(
       <div>
-        BandsContainer
+        <BandInput 
+          addBand={this.props.addBand}
+          />
+          {this.renderBands(this.props.bands)}
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapStateToProps = state => {
+  return {
+    bands:state.bands
+  }
+}
+
+const mapDispatchToProps = dispatch => ({ addBand: band => dispatch({ type: "ADD_BAND", band }) })
+
+export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer)
